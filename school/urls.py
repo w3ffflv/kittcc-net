b1e40384e5e62107cf001f37cb4aaf779ffc20bd
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 02:22:33 +0300

    yes

--- school/urls.py
+++ school/urls.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from django.contrib.auth import views as auth_view
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
-    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+     path('', auth_views.LoginView.as_view(template_name = "index.html"), name="index"),
+     path('accounts/logout', auth_views.LogoutView.as_view(), name="logout"),
+     path('about/', views.about, name="about"),
+     path('contact/', views.contact, name="contact"),
+     path('accounts/profile/', views.ProfileView.as_view(), name="profile"),
+     path('home/', views.home, name="home"),
 
+     # Django Auth
+     path('accounts/login', auth_views.LoginView.as_view(template_name ="index.html"), name='login'),
   
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- school/views.py
+++ school/views.py
@@ -1,33 +1,28 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from .forms import UserRegisterForm
+from curses.ascii import HT
+from urllib import request
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 from django.template import loader
-from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     template = loader.get_template('home.html')
-    return HttpResponse(template.render({}, request))
-
-
-def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Hi {username}, your account was created successfully')
-            return redirect('home')
-    else:
-        form = UserRegisterForm()
-
-    template = loader.get_template('register.html')
-    return HttpResponse(template.render({}, request))
-
-
+    return HttpResponse(template.render({},request))
 @login_required()
-def profile(request):
-    template = loader.get_template('profile.html')
+def index(request):
+    template = loader.get_template('index.html')
+    
     return HttpResponse(template.render({}, request))
+
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render({}, request))
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render({}, request))
+
+class ProfileView(LoginRequiredMixin,TemplateView):
+    template_name = 'accounts/profile.html'
