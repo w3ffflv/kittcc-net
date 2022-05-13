51a31a22bf29bd5cd78700ec4756b40ed03c5f42
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 02:00:52 +0300

    new

--- school/models.py
+++ school/models.py
@@ -1,2 +1,3 @@
-from os import access
 from django.db import models
+
+# Create your models here.
--- school/views.py
+++ school/views.py
@@ -1,33 +1,33 @@
-from curses.ascii import HT
-from urllib import request
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.http import HttpResponse
-from django.views.generic.base import TemplateView
 from django.template import loader
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     template = loader.get_template('home.html')
-    return HttpResponse(template.render({},request))
+    return HttpResponse(template.render({}, request))
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your account was created successfully')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    template = loader.get_template('register.html')
+    return HttpResponse(template.render({}, request))
+
+
 @login_required()
-def index(request):
-    template = loader.get_template('index.html')
-    
+def profile(request):
+    template = loader.get_template('profile.html')
     return HttpResponse(template.render({}, request))
-
-def about(request):
-    template = loader.get_template('about.html')
-    return HttpResponse(template.render({}, request))
-
-def contact(request):
-    template = loader.get_template('contact.html')
-    return HttpResponse(template.render({}, request))
-
-class ProfileView(LoginRequiredMixin,TemplateView):
-    template_name = 'accounts/profile.html'
-    
-         
-        
-
-   
