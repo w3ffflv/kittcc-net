84e126096cc6df65f11c05e41ea71d53f3cc54f7
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-13 07:25:24 +0300

    1

--- school/urls.py
+++ school/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
      path('', auth_views.LoginView.as_view(template_name = "index.html"), name="index"),
@@ -14,4 +17,4 @@ urlpatterns = [
      path('accounts/login', auth_views.LoginView.as_view(template_name ="accounts/login.html"), name='login'),
      path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
   
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
