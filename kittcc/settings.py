b53bddf0effa1fd646a16a0c3d223b373d625037
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-13 08:22:26 +0300

    o kak

--- kittcc/settings.py
+++ kittcc/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/www/kittcc.net/static/'
-#STATICFILES_DIRS = [   BASE_DIR / "staticfiles",] 
+STATICFILES_DIRS = [   BASE_DIR / "staticfiles",] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
