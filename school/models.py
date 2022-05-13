0d9096e140ed9a6b35ea004cfe9790ba12645621
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 02:42:43 +0300

    test

--- school/models.py
+++ school/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Details(models.Model):
+    skola = models.CharField(max_length=205, default='', null=False)
+    skolenuskaits = models.CharField(max_length=222, default='', null=False)
--- school/views.py
+++ school/views.py
@@ -5,11 +5,31 @@ from django.views.generic.base import TemplateView
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from .models import Details
 
 
 def home(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render({},request))
+
+     if request.method == 'POST':
+        skola = request.POST['skola']
+        skolenuskaits = request.POST['skolenuskaits']
+        #Creating the Object of record every time user clicks on 'Add Data'
+        obj = Details()
+        obj.skola = skola
+        obj.skolenuskaits = skolenuskaits
+        obj.save()
+        
+    #Fetching the details and saving in an dictionary
+        from django.core import serializers
+        data = serializers.serialize("python",Details.objects.all());
+
+    #Dictionary to store the data and send it back to HTML format
+        context = {
+            'data':data,
+        }
+        template = loader.get_template('home.html')
+        return HttpResponse(template.render({},request))
+
 @login_required()
 def index(request):
     template = loader.get_template('index.html')
