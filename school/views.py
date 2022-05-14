4efc9edae2601834f148483a015ebb98cb16c016
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 03:15:12 +0300

    twe

--- school/views.py
+++ school/views.py
@@ -1,19 +1,14 @@
-from curses.ascii import HT
-from urllib import request
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
-
 
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render({},request))
-@login_required()
+
 def index(request):
     template = loader.get_template('index.html')
-    
     return HttpResponse(template.render({}, request))
 
 def about(request):
