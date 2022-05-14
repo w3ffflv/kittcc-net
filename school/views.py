80364e0dcda40abe368281c277eb3581205f82ce
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 03:30:26 +0300

    yes

--- school/views.py
+++ school/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Post
-from django.shortcuts import render
 
 
 def home(request):
