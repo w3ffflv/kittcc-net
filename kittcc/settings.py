c2be90f712ebfbf2ec9c974d11f2f04d97f66182
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 03:16:57 +0300

    yeseses

--- kittcc/settings.py
+++ kittcc/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/www/kittcc.net/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
