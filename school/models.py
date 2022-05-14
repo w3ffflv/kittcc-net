3d904aca365ae08a4e36690d50a7a5dbb7b4c75b
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 04:24:06 +0300

    yes

--- school/models.py
+++ school/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class School(models.Model):
+    skola = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}'
--- school/views.py
+++ school/views.py
@@ -1,10 +1,16 @@
+from tkinter import Scrollbar
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
+from school.models import School
 
 
 def home(request):
+    skola = School.objects.all()
+    context = {
+        'data': skola
+    }
     template = loader.get_template('home.html')
     return HttpResponse(template.render({},request))
 
@@ -21,6 +27,7 @@ def contact(request):
     return HttpResponse(template.render({}, request))
 
 class ProfileView(LoginRequiredMixin,TemplateView):
+    
     template_name = 'accounts/profile.html'
     
 
