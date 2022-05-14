8b0f5a9a87e187ce4684097d106e5d749da22106
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 06:04:53 +0300

    y

--- school/models.py
+++ school/models.py
@@ -1,7 +1,2 @@
 from django.db import models
 
-class Schoolsearch(models.Model):
-    skola = models.CharField(max_length=255)
-    novads = models.CharField(max_length=255)
-    skolenuskaits = models.CharField(max_length=255)
-    apestasporcijas = models.CharField(max_length=255)
--- school/views.py
+++ school/views.py
@@ -2,14 +2,13 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
-from school.models import Schoolsearch
+
 
 
 
 def home(request):
-    result = Schoolsearch.objects.all()
     template = loader.get_template('home.html')
-    return HttpResponse(template.render({},request), {"Schoolsearch": result})
+    return HttpResponse(template.render({},request))
 
 def index(request):
     template = loader.get_template('index.html')
