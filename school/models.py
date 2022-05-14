f35d5ff3d200baa035a8f2db3b2bf01aee9ef503
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 06:02:25 +0300

    wr

--- school/models.py
+++ school/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 class Schoolsearch(models.Model):
-    class Meta:
-        db_table="auth_user"
-        unique_together = ['skola', 'novads','skolenuskaits','apestasporcijas']
     skola = models.CharField(max_length=255)
     novads = models.CharField(max_length=255)
     skolenuskaits = models.CharField(max_length=255)
