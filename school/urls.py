0827905e911f02f1014c53e541ec610507bb0ee1
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 03:31:23 +0300

    yes

--- school/urls.py
+++ school/urls.py
@@ -4,7 +4,6 @@ from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import list_posts
 
 
 urlpatterns = [
