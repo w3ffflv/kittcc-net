c7e90e92a7506213acd6e617dab67e78e323635b
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 05:58:20 +0300

    wewe

--- school/models.py
+++ school/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 class Schoolsearch(models.Model):
-    skola = models.CharField(max_length=255)
-    novads = models.CharField(max_length=255)
-    skolenuskaits = models.CharField(max_length=255)
-    apestasporcijas = models.CharField(max_length=255)
 
     class Meta:
         db_table="auth_user"
         managed = False
         unique_together = ['skola', 'novads','skolenuskaits','apestasporcijas']
         
- 
+        skola = models.CharField(max_length=255)
+        novads = models.CharField(max_length=255)
+        skolenuskaits = models.CharField(max_length=255)
+        apestasporcijas = models.CharField(max_length=255)
