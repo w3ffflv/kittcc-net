2124950008bfe3403de7bf1c78462419cb7a9f86
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-14 03:00:40 +0300

    ew

--- kittcc/settings.py
+++ kittcc/settings.py
@@ -144,4 +144,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'social.User'
  
