e0d7ede515f3524fdbfdb82e1361c4517a0349a9
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-15 23:34:21 +0300

    we

--- school/models.py
+++ school/models.py
@@ -1,3 +1,18 @@
+from statistics import mode
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
+class Lietotaji(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=254)
+    password = models.CharField(max_length=254)
+    skola = models.CharField(max_length=254)
+    skolenuskaits = models.CharField(max_length=254)
+    novads = models.CharField(max_length=254)
+    apestasporcijas = models.CharField(max_length=254)
+    pirmdiena = models.CharField(max_length=254)
+    otrdiena = models.CharField(max_length=254)
+    tresdiena = models.CharField(max_length=254)
+    ceturdiena = models.CharField(max_length=254)
+    piekdiena = models.CharField(max_length=254)
 
