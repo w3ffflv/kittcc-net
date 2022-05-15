503725c935bbeb0989347c9fde714f8ce9719aac
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-15 22:49:29 +0300

    12

--- school/urls.py
+++ school/urls.py
@@ -7,16 +7,13 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-     path('', auth_views.LoginView.as_view(template_name = "login.html"), name="login"),
-     path('accounts/logout', auth_views.LogoutView.as_view(), name="logout"),
-     path('about/', views.about, name="about"),
-     path('contact/', views.contact, name="contact"),
-     path('accounts/profile/', views.ProfileView.as_view(), name="profile"),
-     path('home/', views.home, name="home"),
+     path('', views.welcome(), name="welcome"),
+     path('login', views.login(), name="login"),
+     path('register', views.register(), name="register"),
+
 
     
 
      # Django Auth
-     path('accounts/login', auth_views.LoginView.as_view(template_name ="index.html"), name='login'),
   
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
