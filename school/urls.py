d9654cc8043e4287116dcf7f6c3370ad8f552d6d
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-15 23:16:48 +0300

    sd

--- school/urls.py
+++ school/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
      path('', views.welcome, name="welcome"),
-     path('login', views.login, name="login"),
-     path('register', views.register, name="register"),
+     path('login/', views.login, name="login"),
+     path('register/', views.register, name="register"),
 
 
     
